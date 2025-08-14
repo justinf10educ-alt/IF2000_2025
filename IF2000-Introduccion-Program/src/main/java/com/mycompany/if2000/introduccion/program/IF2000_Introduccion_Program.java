b4225518fbdc334082404767fd74d8e4146daf13
unknown <justinf10educ@gmail.com>
2025-08-14 11:54:56 -0600

    Prueba 1

---
+++ IF2000-Introduccion-Program/src/main/java/com/mycompany/if2000/introduccion/program/IF2000_Introduccion_Program.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.if2000.introduccion.program;
+
+/**
+ *
+ * @author Justin Flores
+ */
+public class IF2000_Introduccion_Program {
+    
+}
---
+++ IF2000-Introduccion-Program/src/main/java/com/mycompany/if2000/introduccion/program/IF2000_Introduccion_progra.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.mycompany.if2000.introduccion.program;
+
+/**
+ *
+ * @author Justin Flores
+ */
+public class IF2000_Introduccion_progra {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //Data types
+        
+        String name = "Lionel";
+        int age = 40;
+        float heigh = 40;
+        
+        System.out.println("Hello World");
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        
+        /*
+        Tipos de datos numéricos:
+        int, long, short, float, double numeric, money, decimal
+        
+        Manejo de fechas
+        Date
+        
+        Cadena de caracteres:
+        String
+        
+        Objectos:
+        Object
+        
+        Data Structures:
+        Arrays or Vectors: int[] x;
+                           int[] [] y;
+        True/False:
+        boolean
+        
+        System.out.println("Hola Mundo");
+        System.out.println("Nombre:" + name);
+        //Python:
+        print("Hola Mundo")
+        */
+    
+        
+    }
+    
+}
